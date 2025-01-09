c161dc605bbfd15a94b65f34298b8f140dbd3070
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2025-01-09 14:54:45 +0400

    Fixes and Additions to buildandroid.sh

--- src/buildandroid.sh
+++ src/buildandroid.sh
@@ -5,12 +5,18 @@ if uname == Linux && if uname == Termux; then
     echo "uname()
 {
     Android
-}"
+}" >> ~/.bashrc
 
     pushd `dirname $0`
     . ./thirdparty/src4droid/src4droid.sh
     devtools/bin/vpc /hl2 +everything /mksln everything
     popd
 mv ~/.bashrc-replace ~/.bashrc
-else
-    echo "MacOS & FreeBSD is unsupported"
+elif uname == MINGW64_NT; then
+    echo "Windows is unsupported!!!"
+    exit 1
+elif uname == Darwin; then
+    echo "MacOS unsupported!!!"
+    exit 1
+elif uname == BSD; then
+    echo "BSD is unsupported!!!"
